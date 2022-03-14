78f1a1ef3eb0f1f33b79a0a56bd53fdc8582acb2
Austin Sianis <96740003+NRG1ZE@users.noreply.github.com>
2022-03-14 17:35:48 -0400

    Commit 1st solution to simplifyPath

--- Austin Solutions JS/simplifyPath.js
+++ Austin Solutions JS/simplifyPath.js
@@ -1,4 +1,4 @@
-/* SimplifyPath
+/* SimplifyPath -Medium-
 Given a string path, which is an absolute path (starting with a slash '/') to a file or directory in a Unix-style file system, convert it to the simplified canonical path.
 
 In a Unix-style file system, a period '.' refers to the current directory, a double period '..' refers to the directory up a level, and any multiple consecutive slashes (i.e. '//') are treated as a single slash '/'. For this problem, any other format of periods such as '...' are treated as file/directory names.
@@ -39,4 +39,47 @@ path is a valid absolute Unix path.
  * @param {string} path
  * @return {string}
  */
-var simplifyPath = function (path) {};
+var simplifyPath = function (path) {
+  const regex = /\/+/g;
+  let upPath = path.replace(/\.+/g, "").replace(regex, "/");
+  if (upPath.endsWith("/") && upPath.length > 1) upPath = upPath.slice(0, -1);
+  return upPath;
+};
+
+console.log(simplifyPath("/../"));
+console.log(simplifyPath("/home//foo/"));
+
+/*
+Input:
+"/a/./b/../../c/"
+Output:
+"/a/b/c"
+Expected:
+"/c"
+*/
+
+var simplifyPath = function (path) {
+  // Split path by matching /./ or /
+  let pathArr = path.split(/\/\.\/|\/+/);
+  /* 
+    Eg: "/a/../../b/../c//.//".split(/\/\.\/|\/+/)
+    Gives us  ['', 'a', '..', '..', 'b', '..', 'c', '.', '']
+    */
+
+  //Stack to keep track of path
+  let stack = [];
+  for (let i = 0; i < pathArr.length; i++) {
+    if (pathArr[i]) {
+      //i.e out pathArr !== ""
+      /* 
+            ".." means that we have to go back so we do pop
+            */
+      if (pathArr[i] === "..") stack.pop();
+      /* An empty . in the end is not a valid path so if we do not encounter "."
+            that means that our path is valid so we push it to stack
+            */ else if (pathArr[i] !== ".") stack.push(pathArr[i]);
+    }
+  }
+  // In the end we return our path that we have in stack
+  return "/" + stack.join("/");
+};
