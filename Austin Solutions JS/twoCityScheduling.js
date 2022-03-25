88dbbc332bb2c454d0cc782f5db239d6e7cd8de0
Austin Sianis <96740003+NRG1ZE@users.noreply.github.com>
2022-03-25 13:38:07 -0400

    Commit twoCity Attempt1

--- Austin Solutions JS/twoCityScheduling.js
+++ Austin Solutions JS/twoCityScheduling.js
@@ -55,11 +55,49 @@ POA:
  * @param {number[][]} costs
  * @return {number}
  */
-var twoCitySchedCost = function (costs) {
-  //var for total number of passengers equals cost.length(n) note:each city must have half(n/2)
-  //
+
+var twoCitySchedCost = (costs) => {
+  const allInterviews = costs.length;
+  const fullCity = costs.length / 2;
+
+  let flights = [];
+  for (let i = 0; i <= allInterviews - 1; i++) {
+    console.log({ i });
+    for (let j = 0; j <= costs[i].length - 1; j++) {
+      console.log({ j });
+      flights.push([i, [costs[i][j], j]]);
+      console.log({ flights });
+    }
+  }
+  var cheapestFlight = {
+    cityA: 0,
+    cityB: 0,
+  };
+  cheapestFlights = (flights) => {
+    flights.sort(function (a, b) {
+      a[1][0] - b[1][0];
+    });
+  };
+  console.log(flights);
+
+  if (!(cheapestFlight.cityA === null || undefined));
+  cheapestFlight.cityA = 1;
+  cheapestFlight.cityA++;
+  // return cheapestFlight.cityA;
+  //forEach subArray push each j value with its indexed position and city value to an array [i,[cost[i][j],j]]; at max 200subarrays
+  //[original index postion,[all values for cost, 0 or 1 represents city A or city B ]]
+  //sort by smallest values at array[i][1][0]
+  //if (array[1][1]===0 && objectTracker.cost[0] === fullCity||array[1][1]===1 and objectTracker.cost[1] === fullCity) full break else continue
+  //if object.cost[array[0]]==='' then object.cost[array[0]]= array[1]
+  //if !(object.cost[array[0]]=== '') then continue
+  //object tracker cost:[i,j]
+  //object tracker city:[0=A-counter, 1=B-counter]; tracked by j value
+  //objecttracker.cost === arr.length = costs.length
+  //if abArr[0]+abArr[1] === costs.length return total
+  //total = objecttracker.cost add each cost[0]
+  //Add values till A equals
   //return the minimum Costs where n passengers make it equally to each airport
-  return console.log(costs);
+  //return console.log(costs);
 };
 
 twoCitySchedCost([
